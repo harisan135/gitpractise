79a029d0293ef92ea629c893729eb0cd3c1a004e
Harish <harishmenon4571@gmail.com>
2021-07-07 15:38:32 +0530

    master new changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
 //all python code goes here. 
 var a =1;
-def abc();
-In master branch code;
-pf("in master now") 
-test12121212112212
-new file for v2
-pf("in v2.0")
+function(master code)
+{
+    hello world;
+}
