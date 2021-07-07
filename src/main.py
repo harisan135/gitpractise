57dc14ff415ff71d5d71036990a14c1ef21f7c06
Harish <harishmenon4571@gmail.com>
2021-07-07 12:28:22 +0530

    change done

--- src/main.py
+++ src/main.py
@@ -1,12 +1,8 @@
 //all python code goes here. 
 var a =1;
-<<<<<<< HEAD
-test12121212112212;
 def abc();
 In master branch code;
 pf("in master now") 
-=======
 test12121212112212
 new file for v2
 pf("in v2.0")
->>>>>>> v2.0
